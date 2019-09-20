326ad52816ebde676b046042fd435e04cb91b04c
Nelson Romero <nromero125@gmail.com>
2019-09-20 09:37:19 -0400

    deleted unnecessary commands from run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-chgrp -R www-data storage /app/bootstrap/cache
-chmod -R ug+rwx storage /app/bootstrap/cache
-php /app/artisan migrate
 exec /usr/bin/supervisord --nodaemon -c /etc/supervisor/supervisord.conf
